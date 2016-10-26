ec852418ca5602ec06de45b832e956029921e47b
Dean Panayotov <deanpanayotov@gmail.com>
2016-10-26 14:00:54 +0300

    Cleaned up MelonBarChart.

--- MelonBarChart/melonbarchart/src/main/java/com/melontech/barchart/lib/MelonBarChart.java
+++ MelonBarChart/melonbarchart/src/main/java/com/melontech/barchart/lib/MelonBarChart.java
@@ -45,7 +45,7 @@ public class MelonBarChart extends LinearLayout {
 
     private double min, max;
     private int minPos, maxPos;
-    private double currentScaleMax;
+    private double scaleMax;
     private int barWidth;
     private boolean initialized = false;
     private Animation.AnimationListener animationListener;
@@ -54,20 +54,20 @@ public class MelonBarChart extends LinearLayout {
 
     public MelonBarChart(Context context) {
         super(context);
-        init(context, null);
+        resetChart(context, null);
     }
 
     public MelonBarChart(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context, attrs);
+        resetChart(context, attrs);
     }
 
     public MelonBarChart(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        init(context, attrs);
+        resetChart(context, attrs);
     }
 
-    private void init(Context context, AttributeSet attributeSet) {
+    private void resetChart(Context context, AttributeSet attributeSet) {
         View root = inflate(getContext(), R.layout.view_bar_chart, this);
 
         chartRoot = (LinearLayout) root.findViewById(R.id.chart_root);
@@ -102,42 +102,50 @@ public class MelonBarChart extends LinearLayout {
                     chart.getViewTreeObserver().removeOnGlobalLayoutListener(this);
                 }
                 initialized = true;
-                initNew();
+                resetChart();
             }
         });
     }
 
-    private void initNew() {
-        params.labeledBars.clear();
-        params.highlightedBars.clear();
+    private void resetChart() {
+        animationListener = null;
+        grid.removeAllViews();
+        labels.removeAllViews();
+
         if (values.size() > 0 && initialized) {
-            initializeChart();
+            animationListener = new Animation.AnimationListener() {
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    if (animationListener == this) {
+                        constructLabels();
+                        animationListener = null;
+                    }
+                }
+
+                @Override
+                public void onAnimationStart(Animation animation) {
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+                }
+            };
+            constructBars(animationListener);
             constructBackgroundGrid();
         }
     }
 
-    private void initializeChart() {
-
-        animationListener = null;
+    private void constructBars(Animation.AnimationListener animationListener) {
 
         barWidth = calculateBarWidth();
         resizeChart(barWidth);
 
-        adapter = new BarAdapter(barWidth, currentScaleMax);
-
+        adapter = new BarAdapter(barWidth, scaleMax);
         adapter.setValues(values);
-
-        params.highlightedBars.add(minPos);
-        params.highlightedBars.add(maxPos);
-
-        params.labeledBars.add(minPos);
-        params.labeledBars.add(maxPos);
-
         adapter.setHighlightedBars(params.highlightedBars);
-
         list.setAdapter(adapter);
 
-        animateChart(1000, grid.getMeasuredHeight());
+        animateBars(1000, grid.getMeasuredHeight(), animationListener);
     }
 
     private int calculateBarWidth() {
@@ -151,6 +159,117 @@ public class MelonBarChart extends LinearLayout {
         chart.getLayoutParams().width = newChartWidth + ((FrameLayout.LayoutParams) frame
                 .getLayoutParams()).leftMargin + ((FrameLayout.LayoutParams) frame
                 .getLayoutParams()).rightMargin;
+    }
+
+    private void animateBars(long duration, final int height, Animation.AnimationListener
+            animationListener) {
+
+        list.getLayoutParams().height = 1;
+        list.setVisibility(View.VISIBLE);
+
+        Animation listAnimation = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if (interpolatedTime == 1) {
+                    list.getLayoutParams().height = LayoutParams.MATCH_PARENT;
+                } else {
+                    list.getLayoutParams().height = (int) (height * interpolatedTime);
+                    list.requestLayout();
+                }
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        listAnimation.setDuration(duration);
+        list.startAnimation(listAnimation);
+
+        blank.setVisibility(VISIBLE);
+        blank.getLayoutParams().height = height;
+
+        Animation blankAnimation = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if (interpolatedTime == 1) {
+                    blank.setVisibility(View.GONE);
+                } else {
+                    blank.getLayoutParams().height = height - (int) (height * interpolatedTime);
+                    blank.requestLayout();
+                }
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        blankAnimation.setDuration(duration);
+        blank.startAnimation(blankAnimation);
+
+        blankAnimation.setAnimationListener(animationListener);
+    }
+
+    private void constructBackgroundGrid() {
+        if (params.scaleStep > 0 && scaleMax != 0) {
+            int gridLineCount = (int) (scaleMax / params.scaleStep);
+
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context
+                    .LAYOUT_INFLATER_SERVICE);
+
+            LinearLayout.LayoutParams lineParams = new LinearLayout.LayoutParams
+                    (LinearLayout.LayoutParams.MATCH_PARENT, 0, 1);
+            LinearLayout.LayoutParams dashedLineParams = new LinearLayout.LayoutParams(0,
+                    LayoutParams.MATCH_PARENT, 1);
+            dashedLineParams.setMargins(dpToPx(DASHED_LINE_MARGIN_LEFT), 0, dpToPx
+                    (DASHED_LINE_MARGIN_RIGHT), 0);
+
+            View view, subview;
+            for (int i = gridLineCount - 1; i >= 0; i--) {
+                if (params.dashedLines.contains(i)) {
+                    view = inflater.inflate(R.layout.view_grid_line_dashed, grid, false);
+                    LinearLayout innerFrame = (LinearLayout) view.findViewById(R.id.inner_frame);
+                    for (int j = 0; j < params.fixedDataSetSize; j++) {
+                        subview = inflater.inflate(R.layout.view_dashed_line_segment, innerFrame,
+                                false);
+                        subview.setLayoutParams(dashedLineParams);
+                        innerFrame.addView(subview);
+                    }
+
+                } else {
+                    view = inflater.inflate(R.layout.view_grid_line, grid, false);
+                }
+                view.setLayoutParams(lineParams);
+                grid.addView(view);
+            }
+        }
+    }
+
+    private void constructLabels() {
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context
+                .LAYOUT_INFLATER_SERVICE);
+        TextView textView;
+        for (int position : params.labeledBars) {
+            textView = (TextView) inflater.inflate(R.layout.view_label, labels, false);
+            textView.setText(String.format(Locale.getDefault(), params.labelFormat, values.get
+                    (position)));
+            textView.measure(0, 0);
+            int textViewWidth = textView.getMeasuredWidth();
+            int textViewHeight = textView.getMeasuredHeight();
+            int barHeight = list.getChildAt(position).findViewById(R.id.positive)
+                    .getMeasuredHeight();
+            FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(ViewGroup
+                    .LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(position * barWidth + dpToPx(SIDE_BAR_MARGIN) + Math.round
+                    (barWidth / 2f) - Math.round(textViewWidth / 2f) + ((FrameLayout
+                    .LayoutParams) frame.getLayoutParams()).leftMargin, chart.getMeasuredHeight() -
+                    barHeight - textViewHeight - dpToPx(params.labelMarginBottom), 0, 0);
+            textView.setLayoutParams(layoutParams);
+            labels.addView(textView);
+        }
     }
 
     private void addZeroPadding() {
@@ -187,90 +306,23 @@ public class MelonBarChart extends LinearLayout {
                 maxPos = i;
             }
         }
+
+        params.highlightedBars.add(minPos);
+        params.highlightedBars.add(maxPos);
+
+        params.labeledBars.add(minPos);
+        params.labeledBars.add(maxPos);
     }
 
     private void calculateScale() {
-        currentScaleMax = params.scaleStep != 0 ? ceil(max, params.scaleStep) : max * (1 +
+        scaleMax = params.scaleStep != 0 ? ceil(max, params.scaleStep) : max * (1 +
                 DEFAULT_SCALE_PADDING);
-        if (params.absoluteScaleMax != 0 && currentScaleMax > params.absoluteScaleMax) {
-            currentScaleMax = params.absoluteScaleMax;
+        if (params.absoluteScaleMax != 0 && scaleMax > params.absoluteScaleMax) {
+            scaleMax = params.absoluteScaleMax;
         }
-        if (params.minimumScaleMax != 0 && currentScaleMax < params.minimumScaleMax) {
-            currentScaleMax = params.minimumScaleMax;
+        if (params.minimumScaleMax != 0 && scaleMax < params.minimumScaleMax) {
+            scaleMax = params.minimumScaleMax;
         }
-    }
-
-    private void constructBackgroundGrid() {
-        if (params.scaleStep > 0 && currentScaleMax != 0) {
-            int gridLineCount = (int) (currentScaleMax / params.scaleStep);
-            View view, subview;
-            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context
-                    .LAYOUT_INFLATER_SERVICE);
-
-            LinearLayout.LayoutParams horizontalParams = new LinearLayout.LayoutParams
-                    (LinearLayout.LayoutParams.MATCH_PARENT, 0, 1);
-            LinearLayout.LayoutParams dashedLineParams = new LinearLayout.LayoutParams(0,
-                    LayoutParams.MATCH_PARENT, 1);
-            dashedLineParams.setMargins(dpToPx(DASHED_LINE_MARGIN_LEFT), 0, dpToPx
-                    (DASHED_LINE_MARGIN_RIGHT), 0);
-
-            grid.removeAllViews();
-
-            for (int i = gridLineCount - 1; i >= 0; i--) {
-                if (params.dashedLines.contains(i)) {
-                    view = inflater.inflate(R.layout.view_grid_line_dashed, grid, false);
-                    LinearLayout innerFrame = (LinearLayout) view.findViewById(R.id.inner_frame);
-                    for (int j = 0; j < params.fixedDataSetSize; j++) {
-                        subview = inflater.inflate(R.layout.view_dashed_line_segment, innerFrame,
-                                false);
-                        subview.setLayoutParams(dashedLineParams);
-                        innerFrame.addView(subview);
-                    }
-
-                } else {
-                    view = inflater.inflate(R.layout.view_grid_line, grid, false);
-                }
-                view.setLayoutParams(horizontalParams);
-                grid.addView(view);
-
-            }
-        }
-    }
-
-    private void constructLabels() {
-        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context
-                .LAYOUT_INFLATER_SERVICE);
-        TextView textView;
-        for (int position : params.labeledBars) {
-            textView = (TextView) inflater.inflate(R.layout.view_label, labels, false);
-            textView.setText(String.format(Locale.getDefault(), params.labelFormat, values.get
-                    (position)));
-            textView.measure(0, 0);
-            int textViewWidth = textView.getMeasuredWidth();
-            int textViewHeight = textView.getMeasuredHeight();
-            int barHeight = list.getChildAt(position).findViewById(R.id.positive)
-                    .getMeasuredHeight();
-            FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(ViewGroup
-                    .LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            layoutParams.setMargins(position * barWidth + dpToPx(SIDE_BAR_MARGIN) + Math.round
-                    (barWidth / 2f) - Math.round(textViewWidth / 2f) + ((FrameLayout
-                    .LayoutParams) frame.getLayoutParams()).leftMargin, chart.getMeasuredHeight() -
-                    barHeight - textViewHeight - dpToPx(params.labelMarginBottom), 0, 0);
-            textView.setLayoutParams(layoutParams);
-            labels.addView(textView);
-        }
-    }
-
-    private void clearLabels() {
-        labels.removeAllViews();
-    }
-
-    private int dpToPx(int dp) {
-        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
-    }
-
-    private double ceil(double input, double step) {
-        return Math.ceil(input / step) * step;
     }
 
     private Parameters getAtttributeParameters(Context context, AttributeSet attrs) {
@@ -333,6 +385,9 @@ public class MelonBarChart extends LinearLayout {
 
     public void setValues(List<Double> values) {
         this.values.clear();
+        params.labeledBars.clear();
+        params.highlightedBars.clear();
+
         this.values.addAll(values);
 
         addZeroPadding();
@@ -340,81 +395,21 @@ public class MelonBarChart extends LinearLayout {
         getMinMax();
         calculateScale();
 
-        initNew();
+        resetChart();
     }
 
     public void setDashedGridLines(Set<Integer> dashedLines) {
         params.dashedLines.clear();
         params.dashedLines.addAll(dashedLines);
 
-        initNew();
+        resetChart();
     }
 
-    private void animateChart(long duration, final int height) {
+    private int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
 
-        list.getLayoutParams().height = 1;
-        list.setVisibility(View.VISIBLE);
-
-        Animation listAnimation = new Animation() {
-            @Override
-            protected void applyTransformation(float interpolatedTime, Transformation t) {
-                if (interpolatedTime == 1) {
-                    list.getLayoutParams().height = LayoutParams.MATCH_PARENT;
-                } else {
-                    list.getLayoutParams().height = (int) (height * interpolatedTime);
-                    list.requestLayout();
-                }
-            }
-
-            @Override
-            public boolean willChangeBounds() {
-                return true;
-            }
-        };
-
-        listAnimation.setDuration(duration);
-        list.startAnimation(listAnimation);
-
-        blank.setVisibility(VISIBLE);
-        blank.getLayoutParams().height = height;
-
-        Animation blankAnimation = new Animation() {
-            @Override
-            protected void applyTransformation(float interpolatedTime, Transformation t) {
-                if (interpolatedTime == 1) {
-                    blank.setVisibility(View.GONE);
-                } else {
-                    blank.getLayoutParams().height = height - (int) (height * interpolatedTime);
-                    blank.requestLayout();
-                }
-            }
-
-            @Override
-            public boolean willChangeBounds() {
-                return true;
-            }
-        };
-
-        blankAnimation.setDuration(duration);
-        blank.startAnimation(blankAnimation);
-
-        animationListener = new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                clearLabels();
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                if(animationListener == this) {
-                    constructLabels();
-                }
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-            }
-        };
-        blankAnimation.setAnimationListener(animationListener);
+    private double ceil(double input, double step) {
+        return Math.ceil(input / step) * step;
     }
 }
